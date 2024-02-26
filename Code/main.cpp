43ddf7f5bb3efb929476dbf6ca63b7b41214aed8
BIG_PANDA <viens.louc@hotmail.com>
2024-02-26 10:26:37 -0500

    Update main.cpp
    test

--- Code/main.cpp
+++ Code/main.cpp
@@ -13,5 +13,20 @@ int main() {
 	r1.getPosition().setX(5);
 	cout << r1.getPosition().getX();
 	system("pause");
+	
+	/*TEST*/
+
+	/*TEST*/
+
+	/*TEST*/
+
+	/*TEST*/
+	
+	/*TEST*/
+	/*TEST*/
+	/*TEST*/
+	/*TEST*/
+	/*TEST*/
+	/*TEST*/
 	/*TEST*/
 }
